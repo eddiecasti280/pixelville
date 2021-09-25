b157e8fcb5af68bdb32ded2cc88c94b14f10fbd1
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2021-09-24 23:50:51 -0500

    Update Player.gd
    Implemented squish physics
    
    https://www.youtube.com/watch?v=iJx6uKqufJo

--- Player.gd
+++ Player.gd
@@ -4,12 +4,14 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var score : int = 0
+
 var speed : int = 200
 var jumpForce : int = 450
 var gravity : int = 800
 var vel : Vector2 = Vector2()
 var grounded : bool = false
+var motion : Vector2 = Vector2()
+var motion_previous : Vector2 = Vector2()
 
 onready var sprite = $Sprite
 
@@ -34,3 +36,16 @@ func _process(delta):
 		sprite.flip_h = true
 	elif vel.x > 0:
 		sprite.flip_h = false
+
+	if not is_on_floor():
+		grounded = false
+		$Sprite.scale.y = range_lerp(abs(motion.y), 0, abs(jumpForce), 0.75, 1.75)
+		$Sprite.scale.x = range_lerp(abs(motion.y), 0, abs(jumpForce), 1.25, 0.75)
+
+	if not grounded and is_on_floor():
+		grounded = true
+		$Sprite.scale.x = range_lerp(abs(motion_previous.y), 0, abs(1700), 1.2, 2)
+		$Sprite.scale.y = range_lerp(abs(motion_previous.y), 0, abs(1700), 0.8, 0.5)
+		
+	$Sprite.scale.x = lerp($Sprite.scale.x, 1, 1 - pow(0.01, delta))
+	$Sprite.scale.y = lerp($Sprite.scale.y, 1, 1 - pow(0.01, delta))	
